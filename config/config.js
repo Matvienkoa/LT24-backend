e55824510dc169296897ab2058a510c20a9d3b74
Antho <matvienko.anthony@gmail.com>
2024-04-04 15:20:07 +0200

    update env

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config()
 
-module.exports = new Sequelize('geoloc', 'root', 'Rdwxtdb53', {
-    host: 'localhost',
+module.exports = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'mysql'
 });
